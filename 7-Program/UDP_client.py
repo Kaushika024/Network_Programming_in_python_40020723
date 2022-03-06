136be562b998980c54f9ff79c0028279a3efce4e
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2022-03-06 20:22:50 +0530

    Update UDP_client.py

--- 7-Program/UDP_client.py
+++ 7-Program/UDP_client.py
@@ -1,1 +1,17 @@
+import socket
+import sys
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostbyname(socket.gethostname())
+if len(sys.argv)==2:
+    port = int(sys.argv[1])
+
+s.connect((host,port))
+
+print('hostname takes from the server details')
+print('port as command line argument')
+print(s.recv(1024))
+
+s.close()
 
