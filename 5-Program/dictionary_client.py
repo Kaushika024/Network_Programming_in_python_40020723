70b03fb64488cc49cc9a4d1e838b118b850346f3
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2022-03-06 20:10:23 +0530

    Update dictionary_client.py

--- 5-Program/dictionary_client.py
+++ 5-Program/dictionary_client.py
@@ -1,1 +1,15 @@
+import socket
+import sys
+import json
 
+a = 10
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((socket.gethostname(),2133))
+s.listen(5)
+while True:
+	clt,adr=s.accept()
+	print(f"Connection to ( adr) established")
+	m={1:"Client",2:"Server"}
+	mymsg = json.dumps(m)
+	mymsg = bytes(f'{len(mymsg):<{a}}',"utf-8")+mymsg
+	clt.send(mymsg)
