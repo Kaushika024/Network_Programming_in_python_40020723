1977494452187e3d6e88fc4d4360b8cbfbe1a2ac
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2022-03-06 20:17:33 +0530

    Update list_of_object_json_server.py

--- 6-Program/list_of_object_json_server.py
+++ 6-Program/list_of_object_json_server.py
@@ -1,1 +1,25 @@
+import socket
+import sys
+import json
 
+s = socket.socket()
+print('Connection Established')
+print('Fetching data...')
+
+port = 1500
+address = '127.0.0.1'
+s.bind((address,port))
+
+jsonResult = {"NAME":"KAUSHI","PSNO":"40020723","BU":"EMBEDDED V&V","TRACK":"PYTHON ATF"}
+jsonResult = json.dumps(jsonResult).encode('utf-8')
+
+s.listen(1)
+
+while True:
+    c,addr = s.accept()
+    name = c.recv(1024).decode()
+    print("Connected to ",addr,name)
+    data1 = jsonResult
+    c.send(data1)
+    
+    c.close()
