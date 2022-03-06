4200dcccd88c4c1063409650a7281fd1c7bdef2c
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2022-03-06 20:23:43 +0530

    Update UDP_server.py

--- 7-Program/UDP_server.py
+++ 7-Program/UDP_server.py
@@ -1,1 +1,21 @@
+import socket
+import sys
 
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostbyname(socket.gethostname())
+
+if len(sys.argv)==2:
+    port = int(sys.argv[1])
+
+s.bind((host,port))
+
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+    print('Received Request from', addr)
+    print('hostname takes from the server details')
+    print('port as command line argument')
+    c.send(b'Thanks for connecting')
+    c.close()
