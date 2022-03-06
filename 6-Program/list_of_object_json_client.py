750761e1ffdc6a485a2bc1bf40c0d7ff31a26eab
Kaushika024 <94302959+Kaushika024@users.noreply.github.com>
2022-03-06 20:16:15 +0530

    Update list_of_object_json_client.py

--- 6-Program/list_of_object_json_client.py
+++ 6-Program/list_of_object_json_client.py
@@ -1,1 +1,20 @@
+from re import s
+import socket 
+import sys
 
+c=socket.socket(socket.AF_INET,socket.SOCK_STEARM)
+
+port = 1500
+address = '127.0.0.1'
+c.connect((address,port))
+
+name = input("Enter your name: ")
+c.send(bytes(name,'utf-8'))
+
+#receive the data 
+data2 = c.recv(1024).decode('utf-8')
+#print  the received data
+print(data2)
+
+#close the client
+c.close()
